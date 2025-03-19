5ff3516647abd58822ad766ba277267506a719d4
sean-buchanan <seanbuch@proton.me>
2025-03-19 15:09:58 -0600

    Button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
@@ -29,10 +30,10 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">Home</Link>
+            <Link href={homePath()} className={buttonVariants({ variant: "outline"})}>Home</Link>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">Tickets</Link>
+            <Link href={ticketsPath()} className={buttonVariants({ variant: "outline"})}>Tickets</Link>
           </div>
         </nav>
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
