26e78fae0ac266395e2df6c57571b81815528d85
sean-buchanan <seanbuch@proton.me>
2025-03-19 12:00:04 -0600

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: f:\code\Sandbox\NextJS2025\the-road-to-next-app\tailwind.config.js
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
