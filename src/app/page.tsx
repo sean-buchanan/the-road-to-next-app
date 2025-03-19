eaaf9870fa02bf73848b84ee94112ce9e22b5cd5
sean-buchanan <seanbuch@proton.me>
2025-03-19 11:26:04 -0600

    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,20 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">HomePage</h2>
- 
-      <Link href={ticketsPath()} className="text-sm underline">
-        Go to Tickets
-      </Link>
-    </div>
+    <>
+      <div className="flex-1 flex flex-col gap-y-8">
+        <h2 className="text-3xl font-bold tracking-tight">Home</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="text-sm underline">
+          Go to Tickets
+        </Link>
+      </div>
+    </>
   );
 };
 
