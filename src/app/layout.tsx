50172384d7db3b5070ee93699c3ec8b515f509e7
sean-buchanan <seanbuch@proton.me>
2025-03-19 15:33:47 -0600

    Separator, Icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { LucideKanban } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
@@ -30,7 +31,10 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Link href={homePath()} className={buttonVariants({ variant: "outline"})}>Home</Link>
+            <Link href={homePath()} className={buttonVariants({ variant: "ghost"})}>
+              <LucideKanban />
+              <h1 className="text-lg font-semibold">TicketBounty</h1>
+            </Link>
           </div>
           <div>
             <Link href={ticketsPath()} className={buttonVariants({ variant: "outline"})}>Tickets</Link>
