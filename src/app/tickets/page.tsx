bb6ea0aa4878ca563c7c984bac2096edda273956
sean-buchanan <seanbuch@proton.me>
2025-03-20 15:54:54 -0600

    Feature Folder

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,18 +1,8 @@
 //import clsx from "clsx";
 //import { Separator } from "@radix-ui/react-separator";
-import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 import { Heading } from "@/components/heading";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-
-const TICKET_ICONS = {
-    OPEN: <LucideFileText />,
-    IN_PROGRESS: <LucidePencil />,
-    DONE: <LucideCheckCircle />,
-};
+import { TicketItem } from "@/features/ticket/components/ticket-item";
 
 const TicketsPage = () => {
     return (
@@ -21,20 +11,7 @@ const TicketsPage = () => {
 
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
                 {initialTickets.map((ticket) => (
-                    <Card key={ticket.id} className="w-full max-w-[420px]">
-                        <CardHeader>
-                            <CardTitle className="flex gap-x-2">
-                                <span>{TICKET_ICONS[ticket.status]}</span>
-                                <span className="truncate">{ticket.title}</span>
-                            </CardTitle>
-                        </CardHeader>
-                        <CardContent>
-                            <span className="line-clamp-3 whitespace-break-spaces">{ticket.content}</span>
-                        </CardContent>
-                        <CardFooter>
-                            <Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>    
-                        </CardFooter>
-                    </Card>
+                    <TicketItem key={ticket.id} ticket={ticket} />   
                 ))}
             </div>
         </div>
---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
+
+export const TICKET_ICONS = {
+    OPEN: <LucideFileText />,
+    IN_PROGRESS: <LucidePencil />,
+    DONE: <LucideCheckCircle />,
+};
