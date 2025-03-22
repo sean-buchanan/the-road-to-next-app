a8556274cd7b0fd2c1e2b7b144d55c49a90487df
sean-buchanan <seanbuch@proton.me>
2025-03-22 11:07:33 -0600

    Light & dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,20 @@
 import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "./theme/theme-switcher";
 import { buttonVariants } from "./ui/button";
 
 const Header = () => {
     return (
         <nav className="supports-backdrop-blur:bg-background/60 fixed-left-0 right-0 top-0, z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-            <div>
+            <div className="flex items-center gap-x-2">
                 <Link href={homePath()} className={buttonVariants({ variant: "ghost"})}>
                     <LucideKanban />
                     <h1 className="text-lg font-semibold">TicketBounty</h1>
                 </Link>
             </div>
-            <div>
+            <div className="flex items-center gap-x-2">
+                <ThemeSwitcher />
                 <Link href={ticketsPath()} className={buttonVariants({ variant: "default"})}>Tickets</Link>
             </div>
         </nav>
