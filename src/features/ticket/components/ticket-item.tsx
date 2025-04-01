08015cf31a062fa8614c92a6ed62977385a8ee91
sean-buchanan <seanbuch@proton.me>
2025-03-31 19:00:40 -0600

    Fixes

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -4,51 +4,62 @@ import { LucideArrowUpRightFromSquare, LucideTrash } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { deleteTicket } from "@/features/ticket/actions/delete-ticket";
-import { TICKET_ICONS } from "@/features/ticket/constants";
 import { ticketPath } from "@/paths";
+import { deleteTicket } from "../actions/delete-ticket";
+import { TICKET_ICONS } from "../constants";
 
 type TicketItemProps = {
-    ticket: Ticket;
-    isDetail?: boolean;
+  ticket: Ticket;
+  isDetail?: boolean;
 };
 
 const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
-    const detailButton = (
-        <Button asChild variant="outline" size="icon">
-            <Link href={ticketPath(ticket.id)}>
-                <LucideArrowUpRightFromSquare className="h-4 w-4"/>
-            </Link>
-        </Button>
-    );
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link href={ticketPath(ticket.id)}>
+        <LucideArrowUpRightFromSquare className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
 
-    const deleteButton = (
-        <form action={deleteTicket.bind(null, ticket.id)}>
-            <Button variant="outline" size="icon"> 
-                <LucideTrash className="h-4 w-4" /> 
-            </Button>    
-        </form>
-    );
+  const deleteButton = (
+    <form action={deleteTicket.bind(null, ticket.id)}>
+      <Button variant="outline" size="icon">
+        <LucideTrash className="h-4 w-4" />
+      </Button>
+    </form>
+  );
 
-    return (
-        <div className={clsx("w-full max-w-[420px] flex gap-x-1", { "max-w-[580x]": isDetail, "max-w-[420px]": !isDetail, })}>
-            <Card className="w-full">
-                <CardHeader>
-                    <CardTitle className="flex gap-x-2">
-                        <span>{TICKET_ICONS[ticket.status]}</span>
-                        <span className="truncate">{ticket.title}</span>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <span className={clsx("whitespace-break-spaces", {"line-clamp-3": !isDetail,})}>{ticket.content}</span>
-                </CardContent>
-            </Card>
+  return (
+    <div
+      className={clsx("w-full flex gap-x-1", {
+        "max-w-[580px]": isDetail,
+        "max-w-[420px]": !isDetail,
+      })}
+    >
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <span
+            className={clsx("whitespace-break-spaces", {
+              "line-clamp-3": !isDetail,
+            })}
+          >
+            {ticket.content}
+          </span>
+        </CardContent>
+      </Card>
 
-            <div className="flex flex-col gap-y-1">
-                {isDetail ? detailButton : deleteButton}
-            </div>
-        </div>
-    );
+      <div className="flex flex-col gap-y-1">
+        {isDetail ? deleteButton : detailButton}
+      </div>
+    </div>
+  );
 };
 
 export { TicketItem };
