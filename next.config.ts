b864d16309e1e6e07408ca099a93083cae685d38
sean-buchanan <seanbuch@proton.me>
2025-03-31 19:01:57 -0600

    Undo caching

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    staleTimes: {
-      dynamic: 30,
-    },
-  },
+  // experimental: {
+  //   staleTimes: {
+  //     dynamic: 30,
+  //   },
+  // },
 };
 
 export default nextConfig;
