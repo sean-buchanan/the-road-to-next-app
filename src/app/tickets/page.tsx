2b221673140c0f6031e77ce7f632360050eeee3a
sean-buchanan <seanbuch@proton.me>
2025-06-15 13:11:44 -0600

    Create Ticket Form. <CardCompact>

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,12 +6,21 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Heading } from "@/components/heading";
 import { Placeholder } from "@/components/placeholder";
 import { Spinner } from "@/components/spinner";
+import { CardCompact } from "@/features/ticket/components/card-compact";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = async () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All yhour tickets at one place." />
+
+            <CardCompact
+                title="Create Ticket"
+                description="A new ticket will be created"
+                className="w-full max-w-[420px] self-center"
+                content={<TicketCreateForm />}
+            />
 
             <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
                 <Suspense fallback={<Spinner />}>
---
+++ src/features/ticket/components/card-compact.tsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+type CardCompactProps = {
+    title: string;
+    description: string;
+    className?: string;
+    content: React.ReactNode;
+    footer?: React.ReactNode;
+  };
+  
+  const CardCompact = ({
+    title,
+    description,
+    className,
+    content,
+    footer,
+  }: CardCompactProps) => {
+    return (
+      <Card className={className}>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent>{content}</CardContent>
+        {footer && (
+          <CardFooter className="flex justify-between">{footer}</CardFooter>
+        )}
+      </Card>
+    );
+  };
+  
+  export { CardCompact };
+  
